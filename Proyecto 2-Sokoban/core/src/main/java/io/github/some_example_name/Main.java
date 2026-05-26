4a1b0b2870a4a3dfafb67c81cf8580ee4c47778b
Adri-rodri <adrian08rmicloud.com@gmail.com>
2026-05-26 17:24:03 -0600

    Se agrego visual del primer nivel, falta detectar collision!!

--- Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Main.java
+++ Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Main.java
@@ -5,17 +5,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
-    private Texture image,playerSheet;
+    private Texture image,playerSheet,sheetTiles;
+    private TextureRegion wall,box,objetivo,piso;
     ShapeRenderer shape;
     private Player player;
     OrthographicCamera camera;
     char[][] level;
+    boolean initPlayer=false;
     
 
     @Override
@@ -25,6 +28,11 @@ public class Main extends ApplicationAdapter {
         batch = new SpriteBatch();
         image = new Texture("fondo.png");
         playerSheet=new Texture("playerSheet.png");
+        sheetTiles=new Texture("sheetTiles.png");
+        wall=new TextureRegion(sheetTiles,0,2*80,80,80);
+        box=new TextureRegion(sheetTiles,1*80,1*80,80,80);
+        piso=new TextureRegion(sheetTiles,2*80,0*80,80,80);
+        objetivo=new TextureRegion(sheetTiles,3*80,1*80,80,80);
         shape=new ShapeRenderer();
         player= new Player(0,0);
         player.cargarSprites(playerSheet, playerSheet, playerSheet);
@@ -78,9 +86,36 @@ public class Main extends ApplicationAdapter {
     }
     public void dibujarTexturas(SpriteBatch batch){ //dibujar todas las texturas aqui
         batch.draw(image, 0, 0,832,640);
-        player.dibujarPlayer(batch);
+        
+        
         for(int i=0;i<level.length;i++){
+            for(int j=0;j<level[i].length;j++){
+                int yPos= (level.length-1-i)*Constantes.TILE_SIZE;
+                switch(level[i][j]){
+                    case 'a':
+                        batch.draw(piso, j*Constantes.TILE_SIZE, yPos,Constantes.TILE_SIZE,Constantes.TILE_SIZE);
+                        break;
+                    case 'w':
+                        batch.draw(wall, j*Constantes.TILE_SIZE,yPos,Constantes.TILE_SIZE,Constantes.TILE_SIZE);
+                        break;
+                    case 'b':
+                        batch.draw(box, j*Constantes.TILE_SIZE, yPos,Constantes.TILE_SIZE,Constantes.TILE_SIZE);
+                        break;
+                    case 'x':
+                        batch.draw(objetivo,j*Constantes.TILE_SIZE, yPos,Constantes.TILE_SIZE,Constantes.TILE_SIZE);
+                        break;
+                    case 'p':
+                        batch.draw(piso, j*Constantes.TILE_SIZE, yPos,Constantes.TILE_SIZE,Constantes.TILE_SIZE);
+                        if(!initPlayer){
+                            player.x=j*Constantes.TILE_SIZE;
+                            player.y=yPos;
+                            initPlayer=true;
+                        }
+                        break;
+                }
+            }
         }
+        player.dibujarPlayer(batch);
     }
     public void logic(){//aqui se configura toda la logicaa del juego
         player.tecladoInput();
