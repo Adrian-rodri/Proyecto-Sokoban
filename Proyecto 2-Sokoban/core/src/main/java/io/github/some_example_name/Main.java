d59689e2c8d852da9b9c451236234e5cfa8b4bf7
Adri-rodri <adrian08rmicloud.com@gmail.com>
2026-05-28 19:30:28 -0600

    Se agregaron mas Mockups para los menus y el tutorial

--- Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Main.java
+++ Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Main.java
@@ -5,6 +5,6 @@ import com.badlogic.gdx.Game;
 public class Main extends Game {
     @Override
     public void create() {
-        setScreen(new MenuScreen(this));
+        setScreen(new GameScreen(this));
     }
 }
