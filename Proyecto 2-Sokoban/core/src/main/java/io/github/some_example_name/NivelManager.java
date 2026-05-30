389efefa34a637a0602e4347c62019e753256ba5
Adri-rodri <adrian08rmicloud.com@gmail.com>
2026-05-30 10:19:56 -0600

    Inicio de logica de lectura de files .txt para los levels

--- Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/NivelManager.java
+++ Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/NivelManager.java
@@ -1,25 +1,51 @@
 package io.github.some_example_name;
 
+import com.badlogic.gdx.Gdx;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.util.ArrayList;
+
 public class NivelManager implements Gestionable<Nivel>{
+    ArrayList<Nivel> arrayLevels= new ArrayList<>();
+    RandomAccessFile indexFile;
     
+    
+    public int getIndex() throws IOException{
+        boolean esNuevo=!Gdx.files.local("index.skb").exists();
+        String ruta= Gdx.files.local("index.skb").file().getAbsolutePath();
+        indexFile= new RandomAccessFile(ruta,"rw");
+        if(esNuevo || indexFile.length()==0){
+            indexFile.seek(0);
+            indexFile.writeInt(0);
+            return 0;
+        }else{
+            indexFile.seek(0);
+            int indice= indexFile.readInt();
+            indexFile.seek(0);
+            indexFile.writeInt(indice+1);
+            return indice;
+        }
+        
+    }
     @Override
     public Nivel getActual() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return new Nivel(null,1);
     }
 
     @Override
     public void cargar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
     @Override
     public void guardar() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        
     }
 
     @Override
     public int getCantidad() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        return 1;
     }
     
 }
