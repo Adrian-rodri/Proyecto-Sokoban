9edce7f4f73f8080e40b61da2f4c15bec5ab1227
Adri-rodri <adrian08rmicloud.com@gmail.com>
2026-06-09 11:43:52 -0600

    Texturas nuevas aplicadas

--- Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/TileType.java
+++ Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/TileType.java
@@ -4,10 +4,10 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public enum TileType {
-    WALL(0,5),PISO(2,0),META(1,5),AIRE(2,3),BOX_EN_SU_LUGAR(2,2)
-    ,BOX(1,1),BOX_1(0,3),BOX_2(0,4),Meta_1(1,4),Meta_2(1,3);
+    WALL(0,0),PISO(0,1),META(0,2),AIRE(0,5),BOX_EN_SU_LUGAR(0,4)
+    ,BOX(0,3),BOX_1(0,3),BOX_2(0,4),Meta_1(1,4),Meta_2(1,3);
     
-    private static int PIXELS_TILE=80;
+    private static int PIXELS_TILE=32;
     
     private static Texture sheetTiles= null;
     private TextureRegion textura;
