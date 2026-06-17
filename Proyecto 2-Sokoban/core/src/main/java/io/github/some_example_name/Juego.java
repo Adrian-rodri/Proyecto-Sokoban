2ffef48923f357f1235053d56eabb51f53bf62fa
JeanNajar <jean_carlos@unitec.edu>
2026-06-16 22:25:04 -0600

    Implementacion de clase Abstracta juego

---
+++ Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Juego.java
@@ -0,0 +1,14 @@
+package io.github.some_example_name;
+
+import com.badlogic.gdx.Game;
+
+public abstract class Juego extends Game {
+
+    @Override
+    public void create() {
+    }
+    @Override
+    public void dispose() {
+        super.dispose();
+    }
+}
--- Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Main.java
+++ Proyecto 2-Sokoban/core/src/main/java/io/github/some_example_name/Main.java
@@ -3,18 +3,29 @@ package io.github.some_example_name;
 import io.github.some_example_name.game.NivelManager;
 import io.github.some_example_name.screens.LoginScreen;
 import io.github.some_example_name.game.PlayerManager;
-import com.badlogic.gdx.Game;
+import io.github.some_example_name.util.AutoSaver;
 
-public class Main extends Game {
+public class Main extends Juego {
     public PlayerManager playerManager;
     public NivelManager nivelManager;
+    public AutoSaver autoSaver;
     public String idiomaGlobal= "espanol";
     @Override
     public void create() {
         playerManager = new PlayerManager();
         nivelManager = new NivelManager();
-        
         nivelManager.cargar();
+        autoSaver = new AutoSaver(playerManager);
+        autoSaver.start();
+
         setScreen(new LoginScreen(this));
+    }
+    @Override
+    public void dispose() {
+
+        if (autoSaver != null) {
+            autoSaver.stop();
+        }
+        super.dispose();
     }
 }
